76cdfdf8528476a74c83484437369934ac38536c
salman90 <salmansalem9022@gmail.com>
2021-08-09 14:07:34 -0700

    added post routes, and added validation

--- sample.test.js
+++ sample.test.js
@@ -1,8 +1,8 @@
 const servers = require("./server"); 
 const request = require("supertest");
 const db = require("./app/models/")
-const MalwareURL = db.malware;
-const Op = db.Sequelize.Op;
+const MalwareURL = db.testDB.malware;
+const Op = db.testDB.Sequelize.Op;
 
 
 describe('test that get route is working for all instances of serever', () => {
@@ -38,25 +38,37 @@ describe('test that get route is working for all instances of serever', () => {
 
 describe("testing that proxy app is send request to different servers",() => {
 
-    beforeAll(() => {
-        
-    });
 
-    it("the request url should equal to one of the server ports", async () => {
-        const {body}  =  await request(servers.proxyApp)
+    it("the server should return host for the server", async () => {
+        const { body } = await request(servers.proxyApp) 
             .get("/urlinfo/1/www.facebook.com")
-             expect(body).toEqual({
-                 message: 'URL is not safe to visit',
-                 hostname: "www.facebook.com",
-                 path: "",
-                 port: "",
-                 url: "www.facebook.com",
-                 queryString: {},
-             });
-
-    });
+        const serverHosts = ['localhost:9000', 'localhost:1000', 'localhost:8000'];
+        const exists = serverHosts.includes(body.serverHost);
+        expect(exists).toBeTruthy();
+    })
 
 })
+
+
+// need to make test database 
+describe("test database create and fetch data", () => {
+
+    it("the res body should return json object indicate that the url is not safe if exists in database", async () => {
+
+        const { body } = await request(servers.proxyApp)
+            .get("/urlinfo/1/www.facebook.com")
+        expect(body.message).toEqual('URL is not safe to visit');
+
+    });
+
+
+    it("the res body should return json object indicate that the url is safe if exists in database", async () => {
+        const { body } = await request(servers.proxyApp)
+            .get("/urlinfo/1/www.example.com")
+        expect(body.message).toEqual('URL is safe to be visit');
+    });
+})
+
 
 
 
--- server.js
+++ server.js
@@ -1,52 +1,28 @@
-const express = require('express');
-const http = require('http');
-const httpProxy = require('http-proxy');
+const express  = require('express');
 const app      = express();
 const app2     = express();
 const app3     = express();
 const proxyApp = express();
-const mysql = require("mysql2");
-const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
-var corsOptions = {
-    origin: "http://localhost:8081"
-};
 
-app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app2.use(express.json());
+app2.use(express.urlencoded({ extended: true }));
+app3.use(express.json());
+app3.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 
 db.DevDB.sequelize.sync();
-require("./app/routes/malware.routes")(app);
+
+require("./app/routes/malware.path:1000.routes")(app);
+require("./app/routes/malware.path:9000.routes")(app3);
 require("./app/routes/malware.routes")(app2);
-require("./app/routes/malware.routes")(app3);
-
-
-
-const targets = [
-    'http://localhost:8000',
-    'http://localhost:9000',
-    'http://localhost:1000'
-]
-
-// const proxyServer = http.createServer(
-//                         function (req, res) {
-//                         setTimeout(function () {
-//                             proxy.web(req, res, {
-//                                 target: targets[Math.floor(Math.random() * 3)]
-//                             });
-//                         }, 50);
-//                     }).listen(8008);
-
-// http.createServer(function (req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
-//     res.end();
-// }).listen(9008);
+require("./app/routes/malware.proxy.routes")(proxyApp);
 
 const proxyPort = 8007;
 const port8000 = 8000;
@@ -54,36 +30,6 @@ const port9000 = 9000;
 const port1000 = 1000;
 
 
-
-
-const customRouter = function (req) {
-    return targets[Math.floor(Math.random() * 3)];
-}
-
-const rewriteFn = function (path, req) {
-    let hostname    = req.params.hostname;
-    let pathEnd     = req.params[0];
-    if(pathEnd){
-        return path.replace(`/urlinfo/1/${hostname}/${pathEnd}`, `/urlinfo/1/${hostname}/${pathEnd}`);
-    }else{
-        return path.replace(`/urlinfo/1/${hostname}`, `/urlinfo/1/${hostname}`);
-
-    }
-};
-
-const options = {
-    target: 'http://localhost:8000/', // target host
-    router: customRouter,
-    changeOrigin: true, // needed for virtual hosted sites
-    pathRewrite: rewriteFn, // rewrites our endpoints to '' when forwarded to our target
-}
-
-
-
-const exampleProxy = createProxyMiddleware(options);
-
-proxyApp.get("/urlinfo/1/:hostname/*", exampleProxy);
-proxyApp.use("/urlinfo/1/:hostname", exampleProxy);
 
 proxyApp.listen(proxyPort, () => {
     console.log(`listing on port ${proxyPort}...`);
